ed5bf67fb8177201190ef0bcebb996b13d1fcef1
NoahKastin <64390822+NoahKastin@users.noreply.github.com>
2026-01-25 17:51:09 -0500

    Initial Commit

---
+++ Master DungeonTests/Master_DungeonTests.swift
@@ -0,0 +1,17 @@
+//
+//  Master_DungeonTests.swift
+//  Master DungeonTests
+//
+//  Created by Noah Kastin on 1/25/26.
+//
+
+import Testing
+@testable import Master_Dungeon
+
+struct Master_DungeonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
